ae9ca16f734edbd317a99ebf245997f8cb0e83ec
mnievesx <mrnie@amazon.com>
2022-02-15 12:40:36 -0800

    Maria's initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h1>Meri can push to the repo, again</h1>
       <h1>Percell can push to repo</h1>
       <h1>Justin can push to repo too</h1>
+      <h1>Maria can push to repo too</h1>
     </div>
   );
 }
