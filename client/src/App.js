589943a4f5d294eed8a7b5d86428a3e25cc28250
SLMeri <74334648+SLMeri@users.noreply.github.com>
2022-02-16 07:55:31 -0800

    adding files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div className="App">
       <h1>Meri can push to the repo, again</h1>
+      <h1>Percell can push to repo#</h1>
+      <h1>Justin can push to repo too</h1>
+      <h1>Maria can push to repo too</h1>
     </div>
   );
 }
