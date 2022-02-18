213caa1fe1f0ceae15d9dabc8051e3d8694425d0
SLMeri <74334648+SLMeri@users.noreply.github.com>
2022-02-18 06:01:43 -0800

    last minute changes to the formatting

--- client/src/components/Tutors/Tutor/Tutor.js
+++ client/src/components/Tutors/Tutor/Tutor.js
@@ -17,6 +17,48 @@ import useStyles from "./styles.js";
 const Tutor = ({ tutor, setCurrentId }) => {
   const classes = useStyles();
   return (
+    // <Card className={classes.card}>
+    //   <CardMedia
+    //     className={classes.media}
+    //     image={tutor.selectedFile}
+    //     title={tutor.title}
+    //   />
+    //   <div className={classes.overlay}>
+    //     <Typography variant="h6">{tutor.name}</Typography>
+    //     <Typography variant="body2">
+    //       {moment(tutor.activationDate).fromNow()}
+    //     </Typography>
+    //   </div>
+    //   <div className={classes.overlay2}>
+    //     <Button
+    //       style={{ color: "white" }}
+    //       size="small"
+    //       onClick={() => setCurrentId(tutor._id)}
+    //     >
+    //       <MoreHorizIcon fontSize="default" />
+    //     </Button>
+    //   </div>
+    //   <div className={classes.details}>
+    //     <Typography variant="body2" color="textSecondary">
+    //       {tutor.yearsOfExp}
+    //     </Typography>
+    //   </div>
+    //   <CardContent>
+    //     <Typography className={classes.title} variant="h5" gutterBottom>
+    //       {tutor.subject}
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions className={classes.cardActions}>
+    //     <Button size="small" color="primary" onClick={() => {}}>
+    //       <ThumbUpAltIcon fontSize="small" />
+    //       {tutor.endorsmentCount}
+    //     </Button>
+    //     <Button size="small" color="primary" onClick={() => {}}>
+    //       <DeleteIcon fontSize="small" />
+    //       Delete
+    //     </Button>
+    //   </CardActions>
+    // </Card>
     <Card className={classes.card}>
       <CardMedia
         className={classes.media}
@@ -26,7 +68,7 @@ const Tutor = ({ tutor, setCurrentId }) => {
       <div className={classes.overlay}>
         <Typography variant="h6">{tutor.name}</Typography>
         <Typography variant="body2">
-          {moment(tutor.activationDate).fromNow()}
+          {moment(tutor.createdAt).fromNow()}
         </Typography>
       </div>
       <div className={classes.overlay2}>
@@ -38,21 +80,16 @@ const Tutor = ({ tutor, setCurrentId }) => {
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
-      <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary">
-          {tutor.yearsOfExp}
-        </Typography>
-      </div>
+
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>
-          {tutor.subject}
+        <Typography className={classes.body} variant="body" gutterBottom>
+          {`${"Subject: "}  ${tutor.subject}`}
+        </Typography>
+        <Typography className={classes.body} variant="body" gutterBottom>
+          {`${""} ${"With "}  ${tutor.yearsOfExp} ${" Years of Experience"}`}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
-          <ThumbUpAltIcon fontSize="small" />
-          {tutor.endorsmentCount}
-        </Button>
         <Button size="small" color="primary" onClick={() => {}}>
           <DeleteIcon fontSize="small" />
           Delete
